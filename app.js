cb2cd707370a8aa03b235b3e6953800f733e29f4
King.Zevin <king.zevin@gmail.com>
2020-03-05 09:44:06 +0900

    [v0.7] the basic version - listen and post

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-/*
-* Author: Zevin
-* Project: toServerless - sync
-*/
+const request = require('request')
+const { promisify } = require('util')
 const metrics = require('metrics-sharelatex')
 metrics.initialize('spelling')
 
@@ -14,29 +12,150 @@ if ((Settings.sentry != null ? Settings.sentry.dsn : undefined) != null) {
 metrics.memory.monitor(logger)
 
 const SpellingAPIController = require('./app/js/SpellingAPIController')
+const express = require('express')
+const server = express()
+metrics.injectMetricsRoute(server)
+const bodyParser = require('body-parser')
 const HealthCheckController = require('./app/js/HealthCheckController')
 
-function test(params = {}) {
-  const operation = params.operation || "check";
-  params.user_id = params.user_id || "5dea50e08912bd02137651c2";
-  params.word = params.word || "yess";
-  params.words = params.words || ["yess", "zevina"];
+server.use(bodyParser.json({ limit: '2mb' }))
+server.use(metrics.http.monitor(logger))
 
-  if (operation === "check")
-    return SpellingAPIController.zCheck(params);
-  else if (operation === "learn")
-    return SpellingAPIController.zLearn(params);
-  else if (operation === "unlearn")
-    return SpellingAPIController.zUnlearn(params);
-  else if (operation === "getDic")
-    return SpellingAPIController.zGetDic(params);
-  else if (operation === "deleteDic")
-    return SpellingAPIController.zDeleteDic(params);
-  else if (operation === "status")
-    return { result: { message: "passed", status: "up" } };
-  else if (operation === "healthCheck")
-    return HealthCheckController.healthCheck();
-  else return undefined;
+server.del('/user/:user_id', SpellingAPIController.deleteDic)
+server.get('/user/:user_id', SpellingAPIController.getDic)
+server.post('/user/:user_id/check', SpellingAPIController.check)
+server.post('/user/:user_id/learn', SpellingAPIController.learn)
+server.post('/user/:user_id/unlearn', SpellingAPIController.unlearn)
+server.get('/status', (req, res) => res.send({ status: 'spelling api is up' }))
+
+server.get('/health_check', HealthCheckController.healthCheck)
+
+const settings =
+  Settings.internal && Settings.internal.spelling
+    ? Settings.internal.spelling
+    : undefined
+const host = settings && settings.host ? settings.host : 'localhost'
+const port = settings && settings.port ? settings.port : 3005
+
+server.listen(port, host, function (error) {
+  if (error != null) {
+    throw error
+  }
+  return logger.info(`spelling starting up, listening on ${host}:${port}`)
+})
+
+
+
+function test(params = {}) {
+  const operation = params.operation || "getDic";
+  const user_id = params.user_id || "5dea50e08912bd02137651c2";
+  const word = params.word || "yess";
+  const words = params.words || ["yess", "zevina"];
+
+  const opts = { timeout: 1000 * 20 }
+
+  if (operation === "check") {
+    opts.json = { words, language: 'en' };
+    opts.url = `http://localhost:3005/user/${user_id}/check`;
+    const postReq = promisify(request.post);
+
+    return (async () => {
+      let result = await postReq(opts);
+      let statusCode = result.statusCode;
+      if (statusCode == 500) {
+        return { result: { message: `error processing ${operation} request`, status: "failed" } };
+      }
+      let misspellings = result.body.misspellings;
+      let results = [];
+      for (let index = 0; index < misspellings.length; index++) {
+        results.push({ index: index, word: words[misspellings[index].index], suggestions: misspellings[index].suggestions });
+      }
+      return { result: { misspellings: results, status: "passed" } };
+    })();
+  }
+  else if (operation === "learn") {
+    opts.json = { word, language: 'en' };
+    opts.url = `http://localhost:3005/user/${user_id}/learn`;
+    const postReq = promisify(request.post);
+
+    return (async () => {
+      let result = await postReq(opts);
+      let statusCode = result.statusCode;
+      if (statusCode == 500) {
+        return { result: { message: `error processing ${operation} request`, status: "failed", statusCode } };
+      }
+      return { result: { word, message: "learning passed", status: "passed", statusCode } };
+    })();
+  }
+  else if (operation === "unlearn") {
+    opts.json = { word, language: 'en' };
+    opts.url = `http://localhost:3005/user/${user_id}/unlearn`;
+    const postReq = promisify(request.post);
+
+    return (async () => {
+      let result = await postReq(opts);
+      let statusCode = result.statusCode;
+      if (statusCode == 500) {
+        return { result: { message: `error processing ${operation} request`, status: "failed", statusCode } };
+      }
+      return { result: { word, message: "unlearning passed", status: "passed", statusCode } };
+    })();
+  }
+  else if (operation === "getDic") {
+    opts.url = `http://localhost:3005/user/${user_id}`;
+    const getReq = promisify(request.get);
+
+    return (async () => {
+      let result = await getReq(opts);
+      let statusCode = result.statusCode;
+      if (statusCode == 500) {
+        return { result: { message: `error processing ${operation} request`, status: "failed" }, statusCode };
+      }
+      // return {result: {words:result.body}}
+      return { result: { words:result.body, message: "got dic", status: "passed", statusCode } };
+    })();
+  }
+  else if (operation === "deleteDic") {
+    opts.url = `http://localhost:3005/user/${user_id}`;
+    const delReq = promisify(request.del);
+
+    return (async () => {
+      let result = await delReq(opts);
+      let statusCode = result.statusCode;
+      if (statusCode == 500) {
+        return { result: { message: `error processing ${operation} request`, status: "failed" }, statusCode };
+      }
+      return { result: { message: "got dic", status: "passed", statusCode } };
+    })();
+  }
+  else if (operation === "status") {
+    opts.url = `http://localhost:3005/status`;
+    const getReq = promisify(request.get);
+
+    return (async () => {
+      let result = await getReq(opts);
+      let statusCode = result.statusCode;
+      if (statusCode == 500) {
+        return { result: { message: `error processing ${operation} request`, status: "failed" }, statusCode };
+      }
+      return { result: { message: "up", status: "passed", statusCode } };
+    })();
+  }
+  else if (operation === "healthCheck") {
+    opts.url = `http://localhost:3005/health_check`;
+    const getReq = promisify(request.get);
+
+    return (async () => {
+      let result = await getReq(opts);
+      let statusCode = result.statusCode;
+      if (statusCode == 500) {
+        return { result: { message: "health check failed", status: "failed", statusCode } };
+      }
+      return { result: { message: "health check passed", status: "passed", statusCode } };
+    })();
+  }
+
+
 }
 
 if (!module.parent) {
@@ -44,4 +163,3 @@ if (!module.parent) {
 }
 
 exports.main = test
-
